3fe3bb7755cc4621e3fe16e9b47ad05fd151bcd0
Dan Gamble <dangamble89@gmail.com>
2015-11-10 21:13:33 +0000

    Got a working Users API

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,16 @@
 <?php
 
+Route::get('/', function () {
+    return view('index');
+});
+
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'api'], function () {
+    Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
+    Route::post('authenticate', 'AuthenticateController@authenticate');
 });
